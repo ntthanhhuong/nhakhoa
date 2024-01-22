1c4850284682fae80161839d2340859d2d041663
ntthanhhuong <thanhhuongnguyen260697@gmail.com>
2024-01-22 23:10:58 +0700

    update code

---
+++ js/control.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+$('#slider').owlCarousel({
+	loop: true, // cho lap lai
+	item:1, //xuat hien 1 anh thoi
+	smartSpeed:1000, //toc do thay doi anh
+	autoplay:true, // cho phep tu dong chay
+	autoplayTimeout:5000, // thoi gian cho khi chuyen anh khi chay tu dong
+	nav:true, // cho xuat hien bo nut toi lui
+})
