d89bf18651eb925e9be7624c481d270c228f9114
ntthanhhuong <thanhhuongnguyen260697@gmail.com>
2024-01-23 00:24:21 +0700

    update code

--- js/control.js
+++ js/control.js
@@ -1,9 +1,10 @@
 // JavaScript Document
-$('#slider').owlCarousel({
-	loop: true, // cho lap lai
-	item:1, //xuat hien 1 anh thoi
-	smartSpeed:1000, //toc do thay doi anh
-	autoplay:true, // cho phep tu dong chay
-	autoplayTimeout:5000, // thoi gian cho khi chuyen anh khi chay tu dong
-	nav:true, // cho xuat hien bo nut toi lui
-})
+$('#cn').owlCarousel({
+	loop: true,	// cho lap lại
+	items: 1,	// xuất hiện 1 ảnh thôi
+	smartSpeed: 1000,   	// tốc độ thay đổi ảnh
+	autoplay: true,		// cho phép tự động chạy
+	autoplayTimeout: 5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+	nav: false,	// ẩn xuất hiện bộ nút tới lui 
+	dot: true	// xuất hiện bộ nút tới lui
+});
